08e45c0f0fefbc550231d1a4e72e04532339169b
Mayank Bansal <mayankbansal0915@gmail.com>
2026-06-18 01:20:21 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size())
+            return false;
+
+        map<char, int> freq1;
+        map<char, int> freq2;
+
+        for (int i = 0; i < s.size(); i++) {
+            freq1[s[i]]++;
+            freq2[t[i]]++;
+        }
+
+        return freq1 == freq2;
+    }
+};
